466e789ebee73349395fb4ded4042585bbf8401c
devYuMinKim <gimyumin40@gmail.com>
2023-05-07 10:39:26 +0900

    🐛 fix : Adding foreign keys and correcting errors

--- database/migrations/2023_05_06_063348_create_restaurants_table.php
+++ database/migrations/2023_05_06_063348_create_restaurants_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('restaurants', function (Blueprint $table) {
-            $table->unsignedBigInteger('id');
+            $table->unsignedBigInteger('id')->unique();
             $table->string('title')->default('')->nullable(false);
             $table->text('address')->nullable();
             $table->string('menu_type')->nullable();
--- database/migrations/2023_05_06_074457_create_user_table.php
+++ database/migrations/2023_05_06_074457_create_user_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('email')->unique();
+            $table->unsignedBigInteger('id')->unique();
+            $table->string('email');
             $table->string('password');
             $table->string('nickname');
             $table->timestamps();
