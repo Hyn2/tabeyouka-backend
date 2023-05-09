b48bdcfcbb68b69bc4ab697d30bc26e6ceb313cd
devYuMinKim <gimyumin40@gmail.com>
2023-05-09 11:27:25 +0900

    ✅ test : UserController Test code Error Correction

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,14 +5,16 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Model implements Authenticatable
 {
-    use HasFactory, \Illuminate\Auth\Authenticatable;
+    use HasApiTokens, HasFactory, \Illuminate\Auth\Authenticatable;
 
     protected $table = 'users';
 
     protected $fillable = [
+        'name',
         'email',
         'password',
         'nickname',
