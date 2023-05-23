fdda9c1f087d42869ad6860c87cea3b184579b77
devYuMinKim <gimyumin40@gmail.com>
2023-05-23 15:13:49 +0900

    ✨ feat : Create Community table

---
+++ database/migrations/2023_05_23_053839_create_community_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('community')) {
+            Schema::create('community', function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedBigInteger('author_id')->nullable();
+                $table->foreign('author_id')->references('id')->on('users')->onDelete('set null');
+                $table->string('title');
+                $table->text('text');
+                $table->string('image_file')->nullable();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('community');
+    }
+};
