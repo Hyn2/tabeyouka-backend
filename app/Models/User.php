7c398fd96a323df52b8057b29514f5943a4ed7cb
devYuMinKim <gimyumin40@gmail.com>
2023-05-28 17:04:09 +0900

    🎨 refactor : Modify Logout logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model implements Authenticatable
+class User extends Authenticatable implements MustVerifyEmail, CanResetPasswordContract
 {
-    use HasApiTokens, HasFactory, \Illuminate\Auth\Authenticatable;
+    use HasApiTokens, HasFactory, CanResetPassword, Notifiable;
 
     protected $table = 'users';
 
     protected $fillable = [
-        'name',
         'email',
         'password',
         'nickname',
