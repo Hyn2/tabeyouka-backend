c8a428300e859b1d948553ff9e4867d36d5f8210
devYuMinKim <gimyumin40@gmail.com>
2023-06-04 20:30:20 +0900

    🐛 fix : Fix Comment errors

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,103 +8,57 @@ use App\Models\Community;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index($postId)
     {
-        // not needed, comments should only be displayed with corresponding post
+        $comments = Comment::where('post_id', $postId)->get();
+
+        return response()->json(['comments' => $comments]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        // not needed, comment form should be included in the community.show view
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request, $community)
+    public function store(Request $request, $postId)
     {
         $request->validate([
-            'text' => 'required',
+            'text' => 'required|string',
         ]);
-
-        $post = Community::findOrFail($community);
 
         $comment = Comment::create([
-            'post_id' => $post->id,
-            'author_id' => auth()->id(),
             'text' => $request->text,
+            'author_id' => auth()->id(),
+            'post_id' => $postId,
         ]);
 
-        return redirect()->route('community.show', ['community' => $post->id])->with('success', '댓글이 작성되었습니다.');
+        return response()->json(['comment' => $comment], 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        // not needed, comments should only be displayed with corresponding post
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        // not needed, use an inline editing system in community.show
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $community, Comment $comment)
+    public function update(Request $request, $commentId)
     {
         $request->validate([
-            'text' => 'required',
+            'text' => 'required|string',
         ]);
 
-        $post = Community::findOrFail($community);
+        $comment = Comment::findOrFail($commentId);
+        
+        if (auth()->id() !== $comment->author_id) {
+            return response()->json(['error' => 'You are not authorized to update this comment.'], 403);
+        }
+
         $comment->update([
             'text' => $request->text,
         ]);
 
-        return redirect()->route('community.show', ['community' => $post->id])->with('success', '댓글이 업데이트되었습니다.');
+        return response()->json(['comment' => $comment]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Request $request, $community, Comment $comment)
+    public function destroy($commentId)
     {
-        $post = Community::findOrFail($community);
+        $comment = Comment::findOrFail($commentId);
+
+        if (auth()->id() !== $comment->author_id) {
+            return response()->json(['error' => 'You are not authorized to delete this comment.'], 403);
+        }
+
         $comment->delete();
 
-        return redirect()->route('community.show', ['community' => $post->id])->with('success', '댓글이 삭제되었습니다.');
+        return response()->json(['message' => '댓글이 삭제되었습니다.']);
     }
 }
